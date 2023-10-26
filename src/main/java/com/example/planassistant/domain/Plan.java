93dc3e36e08e26611e41400bf837d56ef742db0c
jihak <jooin2000@naver.com>
2023-10-27 00:56:53 +0900

    commit

--- src/main/java/com/example/planassistant/domain/Plan.java
+++ src/main/java/com/example/planassistant/domain/Plan.java
@@ -1,24 +1,31 @@
 package com.example.planassistant.domain;
 
-
 import com.example.planassistant.common.BaseTimeEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
 @NoArgsConstructor
+@Getter
 // 1:N 매핑
-public class Todo extends BaseTimeEntity {
+public class Plan extends BaseTimeEntity {
+
     @Id
     private Long id;
 
     private String place;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 
-    private Integer priority;
-
+    @Builder
+    public Plan(String place, LocalDateTime startTime, LocalDateTime endTime) {
+        this.place = place;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 }
---
+++ src/main/java/com/example/planassistant/repository/TodoRepository.java
@@ -0,0 +1,4 @@
+package com.example.planassistant.repository;
+
+public interface TodoRepository {
+}
