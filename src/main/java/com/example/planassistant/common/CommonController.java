2a7d9c0b062b126a7ecc44d550c7ffdd8d432b77
DESKTOP-1IBJ60D\JIHAK <jooin2000@naver.com>
2023-10-26 12:02:37 +0900

    Fix

--- src/main/java/com/example/planassistant/common/CommonController.java
+++ src/main/java/com/example/planassistant/common/CommonController.java
@@ -17,33 +17,5 @@ public class CommonController  {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(data);
 
     }
-//    public ResponseEntity SuccessReturn(Object data) {
-//        return ResponseEntity.ok().body(
-//                ResponseDto.builder().statusCode(ApiCode.SUCCESS.getCode()).codeMsg(ApiCode.SUCCESS.getMsg()).data(data).build());
-//    }
-//
-//    public ResponseEntity SuccessReturn() {
-//        return ResponseEntity.ok().body(
-//                ResponseDto.builder().statusCode(ApiCode.SUCCESS.getCode()).codeMsg(ApiCode.SUCCESS.getMsg()).build());
-//    }
-//
-//    public ResponseEntity ErrorReturn(ApiCode apiCode) {
-//        return ResponseEntity.ok().body(
-//                ResponseDto.builder().statusCode(apiCode.getCode()).codeMsg(apiCode.getMsg()).build());
-//    }
-//
-//    public ResponseEntity ErrorReturn(ApiCode apiCode, Object data) {
-//        return ResponseEntity.ok().body(
-//                ResponseDto.builder().statusCode(apiCode.getCode()).codeMsg(apiCode.getMsg()).data(data).build());
-//    }
-//
-//    public ResponseEntity ErrorReturn(int code, String msg) {
-//        return ResponseEntity.ok().body(
-//                ResponseDto.builder().statusCode(code).codeMsg(msg).build());
-//    }
-//
-//    public ResponseEntity ErrorReturn(int code, String msg, Object data) {
-//        return ResponseEntity.ok().body(
-//                ResponseDto.builder().statusCode(code).codeMsg(msg).data(data).build());
-//    }
+
 }
