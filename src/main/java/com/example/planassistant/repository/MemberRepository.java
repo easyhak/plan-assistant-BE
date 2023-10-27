f85fc6d7cb6721119863d05e95548d910b01878a
DESKTOP-1IBJ60D\JIHAK <jooin2000@naver.com>
2023-10-28 00:51:12 +0900

    Fix: memberRepository에서 id를 String값으로 찾게 변경

--- src/main/java/com/example/planassistant/repository/MemberRepository.java
+++ src/main/java/com/example/planassistant/repository/MemberRepository.java
@@ -4,8 +4,9 @@ import com.example.planassistant.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface MemberRepository extends JpaRepository<Member,Long> {
+public interface MemberRepository extends JpaRepository<Member, String> {
     Optional<Member> findByEmail(String email);
     boolean existsByEmail(String email);
 }
