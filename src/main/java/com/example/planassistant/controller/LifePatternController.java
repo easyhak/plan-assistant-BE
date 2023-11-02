458a28e385fda5e76d187fe50b126827356e19a5
DESKTOP-1IBJ60D\JIHAK <jooin2000@naver.com>
2023-11-03 01:46:14 +0900

    Docs: swagger 내용 추가

--- src/main/java/com/example/planassistant/controller/LifePatternController.java
+++ src/main/java/com/example/planassistant/controller/LifePatternController.java
@@ -2,8 +2,9 @@ package com.example.planassistant.controller;
 
 import com.example.planassistant.common.CommonController;
 import com.example.planassistant.dto.lifepattern.LifePatternReqDto;
-import com.example.planassistant.dto.lifepattern.LifePatternResDto;
 import com.example.planassistant.service.LifePatternService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +12,12 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/life")
+@Tag(name = "LifePattern", description = "생활패턴 관련 API")
 public class LifePatternController extends CommonController {
     private final LifePatternService lifePatternService;
     @PostMapping
