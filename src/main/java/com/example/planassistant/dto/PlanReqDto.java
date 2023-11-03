b21cef3136aa9f5ec305e2c95bb0b07ea9006bd0
jihak <jooin2000@naver.com>
2023-11-03 16:02:06 +0900

    Docs

--- src/main/java/com/example/planassistant/dto/PlanReqDto.java
+++ src/main/java/com/example/planassistant/dto/PlanReqDto.java
@@ -1,6 +1,7 @@
 package com.example.planassistant.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -8,14 +9,24 @@ import java.time.LocalDateTime;
 
 @Getter
 @ToString
+@Schema(description = "Plan Response Dto")
 public class PlanReqDto {
 
+    @Schema(description = "장소", example = "중앙대학교")
     private String place;
+    @Schema(description = "내용", example = "캡스톤 회의")
     private String content;
+
+    @Schema(description = "시작 시간", nullable = true, example = "2023-10-12 10:00")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime startTime;
+
+    @Schema(description = "시작 시간", nullable = true, example = "2023-10-12 12:00")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime endTime;
+
+    @Schema(description = "위도", nullable = true, example = "2023-10-12 12:00")
     private String latitude;
+    @Schema(description = "경도", nullable = true, example = "2023-10-12 12:00")
     private String longitude;
 }
--- src/main/java/com/example/planassistant/dto/PlanResDto.java
+++ src/main/java/com/example/planassistant/dto/PlanResDto.java
@@ -2,6 +2,7 @@ package com.example.planassistant.dto;
 
 import com.example.planassistant.domain.Plan;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -9,15 +10,24 @@ import java.time.LocalDateTime;
 
 @Getter
 @ToString
+@Schema(description = "Plan Response Dto")
 public class PlanResDto {
 
+    @Schema(description = "Id", nullable = false, example = "1")
     private Long id;
+    @Schema(description = "장소", example = "중앙대학교")
     private String place;
+    @Schema(description = "위도")
     private String latitude;
+    @Schema(description = "위도")
     private String longitude;
+    @Schema(description = "내용", example = "캡스톤 회의")
     private String content;
+
+    @Schema(description = "시작 시간", nullable = true, example = "2023-10-12 10:00")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime startTime;
+    @Schema(description = "시작 시간", nullable = true, example = "2023-10-12 12:00")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime endTime;
 
