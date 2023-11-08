39b2b35834a26e023a1574141b7154f409a7d046
jihak <jooin2000@naver.com>
2023-11-08 23:59:54 +0900

    Fix: localdatetime으로 변경

--- src/main/java/com/example/planassistant/repository/PlanRepository.java
+++ src/main/java/com/example/planassistant/repository/PlanRepository.java
@@ -4,11 +4,11 @@ import com.example.planassistant.domain.Member;
 import com.example.planassistant.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
     List<Plan> findPlanByMemberOrderByStartTime(Member member);
 
-    List<Plan> findPlanByMemberAndStartTimeBetween(Member member, LocalDate start, LocalDate end);
+    List<Plan> findPlanByMemberAndStartTimeBetweenOrderByStartTime(Member member, LocalDateTime start, LocalDateTime end);
 }
